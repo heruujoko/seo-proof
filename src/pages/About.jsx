f3424861f517eea1ed86a119aea644989e82c394
Heru Joko P. Utomo <heru.utomo@grabtaxi.com>
2025-02-25 13:30:02 +0700

    add json

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+// https://prd-growth.s3.ap-southeast-1.amazonaws.com/public/grabfood-com/merchant-info/6-C221A3JHVKAYRN.json
 
 const About = () => {
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+        const json = await response.json();
+        setData(JSON.stringify(json, null, 2));
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        setData('Error fetching data');
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <div>This IS ABOUT</div>
+    <div>This IS ABOUT {JSON.stringify(data)}</div>
+
   )
 }
 export default About
